757da547655d1e846a29eb16bcb53d87ab45f93c
Rubén Caro <ruben.caro.estevez@gmail.com>
2011-03-20 13:57:49 +0100

    Get dummy_repo zipped and unzipped

--- Gemfile
+++ Gemfile
@@ -4,11 +4,8 @@ gem 'erubis'
 gem 'grit'
 
 group :development, :test do
-#   gem 'capybara'
-#   gem 'launchy'
   gem 'rack-test'
   gem 'test-unit'
-#   gem 'nokogiri'
   gem 'sinatra-reloader'
   gem 'ruby-debug19'
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,28 +2,10 @@
 $LOAD_PATH << './app'
 
 require 'main'
-# require 'capybara'
-# require 'capybara/dsl'
 require 'test/unit'
 require 'rack/test'
 
 ENV['RACK_ENV'] = 'test'
-
-# require 'test_notifier/runner/test_unit'
-# TestNotifier.default_notifier = :notify_send
-
-# # Capybara.app = Sinatra::Application.new
-# Capybara.run_server = true #Whether start server when testing
-# Capybara.default_selector = :css #default selector , you can change to :css
-# Capybara.default_wait_time = 2 #When we testing AJAX, we can set a default wait time
-# Capybara.ignore_hidden_elements = false #Ignore hidden elements when testing, make helpful when you hide or show elements using javascript
-# #Capybara.javascript_driver = :rack_test  #default driver when you using @javascript tag
-# #Capybara.default_driver = :rack_test
-
-# dummy repo for testing
-Git.repos = {
-  :dummy => File.dirname(__FILE__) + '/dummy_repo',
-}
 
 class Test::Unit::TestCase
   include Rack::Test::Methods
@@ -32,11 +14,7 @@ class Test::Unit::TestCase
     Sinatra::Application
   end
 
-#   include Capybara
-  # Capybara.default_driver = :selenium # <-- use Selenium driver
-
   def setup
-#     Capybara.app = Sinatra::Application.new
     build_dummy_repo unless is_dummy_repo?
   end
 
@@ -59,17 +37,14 @@ class Test::Unit::TestCase
     FileTest.exist? "#{File.dirname(__FILE__)}/dummy_repo"
   end
 
+  # dummy repo definition for testing
   def build_dummy_repo
     clean_dummy_repo
-    system <<-BASH
-      cd #{File.dirname(__FILE__)};
-      mkdir dummy_repo;
-      cd dummy_repo;
-      git init;
-      touch README;
-      git add .;
-      git commit -a -m 'Commit readme';
-      BASH
+    # unzip repo, zipped using 'zip -rq dummy_repo.zip dummy_repo'
+    system "cd #{File.dirname(__FILE__)}; unzip dummy_repo.zip"
+    Git.repos = {
+      :dummy => File.dirname(__FILE__) + '/dummy_repo',
+    }
   end
 
   def clean_dummy_repo
